ae0524f89a799f59b57521049c4deb4b8b7ad1c6
KashnykovAnton <kashnikov_anton@yahoo.com>
2022-06-28 01:21:12 +0200

    final commit create-slice

--- src/redux/weather/weather-actions.js
+++ src/redux/weather/weather-actions.js
@@ -3,14 +3,3 @@ import { createAction } from '@reduxjs/toolkit';
 export const addLon = createAction('weather/addLon');
 
 export const addLat = createAction('weather/addLat');
-
-// ------ With AsyncThunk - don't need actions for fetch! ------
-
-// // pending
-// export const fetchWeatherRequest = createAction('weather/fetchWeatherRequest');
-
-// // fulfilled
-// export const fetchWeatherSuccess = createAction('weather/fetchWeatherSuccess');
-
-// // rejected
-// export const fetchWeatherError = createAction('weather/fetchWeatherError');
--- src/redux/weather/weather-operations.js
+++ src/redux/weather/weather-operations.js
@@ -28,36 +28,3 @@ export const fetchWeather = createAsyncThunk(
     }
   },
 );
-
-// ------ NEW, without rejectWithValue
-// export const fetchWeather = createAsyncThunk(
-//     'weather/fetchWeather',
-//     async ({ lat: latitude, lon: longitude }) => {
-//       const response = await fetch(
-//         `${BASE_URL}?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`,
-//       );
-//       const data = await response.json();
-//       return data;
-//     },
-//   );
-
-// ------ OLD, from previous branch
-// import {
-//   fetchWeatherRequest,
-//   fetchWeatherSuccess,
-//   fetchWeatherError,
-// } from './weather-actions';
-
-// export const fetchWeather = (longitude, latitude) => async dispatch => {
-//   dispatch(fetchWeatherRequest());
-
-//   try {
-//     const response = await fetch(
-//       `${BASE_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`,
-//     );
-//     const data = await response.json();
-//     dispatch(fetchWeatherSuccess(data));
-//   } catch (error) {
-//     dispatch(fetchWeatherError(error));
-//   }
-// };
